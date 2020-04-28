e27c204fc471eeacc94a9804a12f34294987aa84
LonlyPan <LonlyPan@gmail.com>
2020-04-28 18:00:32 +0800

    0

--- Random_string/Random_string.cpp
+++ Random_string/Random_string.cpp
@@ -6,7 +6,6 @@
 #include <windows.h>
 #include <fstream>
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 
@@ -45,61 +44,16 @@ char* generateRandomString(int size)
 int main()
 {
     srand(time(NULL));
-    char filename[100];
-    char string_format[100];
-    int string_num[100];
-    char tmp[100];
+    std::cout << "Hello World!\n";
 
-    char c;
-    int i = 0;
-
+    /* 新建文件并打开，每次都要手动修改文件名*/
     ofstream outfile;
-
-    std::cout << "/********** Random String **********/\n\n";
-
-    /*
-    std::cout << "请输入要保存的文件名+扩展名\n";
-    std::cout << "格式示例：123.txt\n";
-
-    while ((c = getchar()) != '\n')
-    {   
-        filename[i] = c;
-        i++;
-    }
-    filename[i] = 0;
-    // 新建文件并打开，每次都要手动修改文件名
-    
-    
-    outfile.open(filename, ios::out | ios::trunc);  // 写模式打开并清除文件内容（若已存在）
-    //std::cout << filename << '\n';
-    */
-
-    std::cout << "请输入字符串格式\n";
-    std::cout << "格式示例: G0 X%s Y%s Z%s E%s . 其中 %s 将被随机字符串替换\n";
-    i = 0;
-    while ((c = getchar()) != '\n')
-    {
-        string_format[i] = c;
-        i++;
-    }
-    string_format[i] = 0;
-
-    std::cout << "请输入字符串长度,与上面的 %s 数量对应\n";
-    std::cout << "格式示例: 1235，则第一个 %s 出会被一个随机字符串替代，第二个 %s 会被两个字符替代...示例如下\n";
-    scanf_s("%d", string_num, sizeof(string_num));
-
-    sprintf_s(tmp, string_format, generateRandomString(2), generateRandomString(2), generateRandomString(2), generateRandomString(2));
-
-
-    std::cout << tmp << '\n';
-
-
-
-    
+    char tmp[100];
+    outfile.open("D:\\G0 X Y Z E.gcode", ios::out | ios::trunc);  // 写模式打开并清除文件内容（若已存在）
 
     for (int i = 0; i < 1000; i++) {
         /* 生成指定格式数据，需手动修改以匹配不同格式 */
-        sprintf_s(tmp, "'string_format'", generateRandomString(2), generateRandomString(2), generateRandomString(2), generateRandomString(2));
+        sprintf_s(tmp, "G0 X%s Y%s Z%s E%s", generateRandomString(2), generateRandomString(2), generateRandomString(2), generateRandomString(2));
         outfile << tmp << endl; // 向文件写入数据
     }
     outfile.close(); // 关闭打开的文件
